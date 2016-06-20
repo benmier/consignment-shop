8c2f1eda590010173cdd33bfe026d1c87b683077
benmier <bencmier@gmail.com>
2016-06-20 16:07:16 -0700

    Consignment Shop application update

--- ConsignmentShopLibrary/Vendor.cs
+++ ConsignmentShopLibrary/Vendor.cs
@@ -11,10 +11,19 @@ namespace ConsignmentShopLibrary
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public double Commission { get; set; }
+        public decimal PaymentDue { get; set; }
 
         public Vendor()
         {
             Commission = .5;
+        }
+
+        public string Display
+        {
+            get
+            {
+                return string.Format("{0} {1} - ${2}", FirstName, LastName, PaymentDue);
+            }
         }
     }
 }
